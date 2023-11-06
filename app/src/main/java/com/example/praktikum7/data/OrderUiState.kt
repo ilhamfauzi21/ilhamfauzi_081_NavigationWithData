45c221e5193c4c230c741c6b70f46cf5d654def8
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-11-06 23:06:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum7/data/OrderUiState.kt
@@ -0,0 +1,7 @@
+package com.example.praktikum7.data
+
+data class OrderUiState (
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String ="",
+)
---
+++ app/src/main/java/com/example/praktikum7/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.praktikum7.data
+
+import com.example.praktikum7.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
