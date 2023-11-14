a6f873affd36a439d3491536f42d74b4e8f64a66
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-11-14 21:36:19 +0700

    add var dalam halamanpelanggan

--- app/src/main/java/com/example/praktikum7/HalamanPelanggan.kt
+++ app/src/main/java/com/example/praktikum7/HalamanPelanggan.kt
@@ -1,9 +1,15 @@
 package com.example.praktikum7
 
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 @Composable
 fun CustomerDetailsScreen(
     onConfirmButtonClicked: (String, String, String) -> Unit,
     onCancelButtonClicked: () -> Unit
-){}
+){ var namaPelanggan by remember { mutableStateOf("") }
+    var nomorTelepon by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+}
