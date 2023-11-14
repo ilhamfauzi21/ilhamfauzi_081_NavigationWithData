570ed70a6e0f2d2977289373b23728c3eb36d0e1
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-11-14 21:29:29 +0700

    add fun costumerdetailScreen

--- app/src/main/java/com/example/praktikum7/HalamanPelanggan.kt
+++ app/src/main/java/com/example/praktikum7/HalamanPelanggan.kt
@@ -1,2 +1,9 @@
 package com.example.praktikum7
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerDetailsScreen(
+    onConfirmButtonClicked: (String, String, String) -> Unit,
+    onCancelButtonClicked: () -> Unit
+){}
