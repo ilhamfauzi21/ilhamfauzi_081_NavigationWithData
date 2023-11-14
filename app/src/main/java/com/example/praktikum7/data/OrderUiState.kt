4bd2f001c46ac14e5b53a5bc1fad10ec613a12a2
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-11-14 21:27:19 +0700

    add vall halamanpelanggan

--- app/src/main/java/com/example/praktikum7/data/OrderUiState.kt
+++ app/src/main/java/com/example/praktikum7/data/OrderUiState.kt
@@ -4,4 +4,7 @@ data class OrderUiState (
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String ="",
+    val namaPelanggan: String = "",
+    val nomorTelepon: String = "",
+    val alamat: String = ""
 )
