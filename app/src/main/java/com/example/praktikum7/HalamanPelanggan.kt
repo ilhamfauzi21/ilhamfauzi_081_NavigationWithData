09e611163ab127726c1c4be1b11c690143690d03
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-11-14 21:38:46 +0700

    membuat colum

--- app/src/main/java/com/example/praktikum7/HalamanPelanggan.kt
+++ app/src/main/java/com/example/praktikum7/HalamanPelanggan.kt
@@ -1,15 +1,28 @@
 package com.example.praktikum7
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 @Composable
 fun CustomerDetailsScreen(
     onConfirmButtonClicked: (String, String, String) -> Unit,
     onCancelButtonClicked: () -> Unit
-){ var namaPelanggan by remember { mutableStateOf("") }
+) {
+    var namaPelanggan by remember { mutableStateOf("") }
     var nomorTelepon by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 16.dp)
+    ) {}
 }
